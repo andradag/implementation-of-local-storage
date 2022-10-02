10e263a57c9666cc7c62266b1847bfe033f343f9
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-10-02 11:24:03 +0100

    target all HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// target all HTML elements
+const todoInput = document.querySelector("#todo-text");
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
+const todoCountSpan = document.querySelector("#todo-count");
