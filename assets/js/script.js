7e84ed46098a324813f7f48a9dfde19fb6d582ac
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-10-02 12:03:49 +0100

    get todos from Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,20 @@ const todoInput = document.querySelector("#todo-text");
 const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const todoCountSpan = document.querySelector("#todo-count");
+
+const todos = [];
+
+function renderToodos() {}
+
+const init = function () {
+  // get "todos" from Local Storage
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+};
+
+function storeTodos() {}
+
+todoForm.addEventListener("submit", function (event) {});
+
+todoList.addEventListener("click", function (event) {});
+
+init();
